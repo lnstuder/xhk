0bffd46549cf751a6550bc5e8683b39ccf6b95bc
fastuder <lian.studer@hslu.ch>
2021-06-30 19:39:18 +0200

    build: precompiled headers

---
+++ xhkpch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// standard library
+#include <iostream>
+
+// data structures
+#include <vector>
+#include <string>
+
+// // chaiscript api
+// #include <chaiscript/chaiscript.hpp>
+
+// project config header
+#include "config.h"
