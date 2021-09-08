dd2452b3f76119dba3a1a5b25d6a92897449d8dd
Lian Studer <ln.studer@protonmail.ch>
2021-09-08 12:05:34 +0200

    feat: add example hotkey procedure

--- config.h
+++ config.h
@@ -1,4 +1,9 @@
 
-struct hotkey_t hotkeys[] = {
-    { XCB_MOD_MASK_CONTROL|XCB_MOD_MASK_SHIFT, XK_space }
+int hahalmao() {
+    puts("OK LOL");
+    return 0;
+} 
+
+hotkey_t hotkeys[] = {
+    { XCB_MOD_MASK_CONTROL|XCB_MOD_MASK_SHIFT, XK_space, &hahalmao }
 };
